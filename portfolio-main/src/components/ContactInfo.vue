06cd2fd6fe52a8ebc92d81fd9640bbcbf42ac542
GilangHyrs <gilanghadyr@gmail.com>
2024-04-06 01:16:12 +0700

    first commit

---
+++ portfolio-main/src/components/ContactInfo.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+    name: 'ContactInfo',
+});
+
+defineProps({
+    header: String,
+});
+
+const data = reactive([
+    {
+        icon: 'mdi-cellphone-sound',
+        title: 'Phone',
+        value: '+62 83843755178',
+        href: 'tel:+62 3843755178',
+        target: null,
+    },
+    {
+        icon: 'mdi-message-text',
+        title: 'Telegram',
+        value: '@Gill_hyrss',
+        href: 'https://t.me/Gill_hyrss',
+        target: '_blank',
+    },
+    {
+        icon: 'mdi-gmail',
+        title: 'Gmail',
+        value: 'gilanghadyr@gmail.com',
+        href: 'mailto:gilanghadyr@gmail.com',
+        target: null,
+    },
+    {
+        icon: 'mdi-github',
+        title: 'GitHub',
+        value: 'GilangHyrs',
+        href: 'https://github.com/GilangHrys',
+        target: '_blank',
+    },
+   
+]);
+</script>
+
+<template>
+    <div class="contact-info">
+        <h5 class="text-h5 text-uppercase text-deep-purple-accent-1">{{ header }}</h5>
+        <div class="contact-info__item mt-3" v-for="(item, i) in data" :key="i">
+            <div class="contact-info__icon">
+                <v-icon size="x-large">{{ item.icon }}</v-icon>
+            </div>
+            <div class="contact-info__content">
+                <span class="text-uppercase text-button">{{ item.title }}</span>
+                <a :href="item.href" :target="item.target">{{ item.value }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.contact-info__item {
+    display: flex;
+    align-items: flex-end;
+}
+
+.contact-info__content {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+}
+</style>
---
+++ portfolio-main/src/components/Timelines.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+    name: 'Timelines',
+});
+
+const timelines = reactive([
+]);
+</script>
+
+<template>
+    <div class="timeline mt-5">
+        <h5 class="text-h5 text-uppercase text-center text-deep-purple-accent-1">Education & work</h5>
+        <v-timeline class="mt-5">
+            <v-timeline-item
+                v-for="timeline in timelines"
+                :key="timeline.id"
+                :dot-color="timeline.color"
+                :icon="timeline.icon"
+                size="large"
+            >
+                <template v-slot:opposite>
+                    <div
+                        :class="`pt-1 headline font-weight-bold text-button text-${timeline.color}`"
+                        v-text="timeline.period"
+                    ></div>
+                </template>
+                <div>
+                    <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${timeline.color}`">
+                        {{ timeline.title }}
+                    </h2>
+                    <h5 :class="`text-button text-${timeline.color}`">{{ timeline.position }}</h5>
+                    <span class="text-subtitle-2">{{ timeline.description }}</span>
+                </div>
+            </v-timeline-item>
+        </v-timeline>
+    </div>
+</template>
